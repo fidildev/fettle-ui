fd0445e94adb7f99e286c69f514f1ee7f2ba3fe1
vrkljam <vrkljam@earthlink.net>
2023-01-26 17:02:20 -0700

    started header-landing-footer pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Landingpage from './components/landingpage/Landingpage';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
         <h1>Hello Fettle User World!!</h1>
+        <Header />
+        <Landingpage />
+        
+        <div >
+          <img src='/assets/redcross.jpg' className='app-image'/>
+        </div>
+        <Footer />
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { BiLogIn } from 'react-icons/bi';
+import { BsSearch } from 'react-icons/bs';
+import './header.css'
+
+
+function Header() {
+  return (
+    <div className='header-container'>
+    
+      <div className="header-left">
+        <span className="logo">
+          <img src="" alt="fettle logo" className='logo-image' />
+          Fettle
+        </span>
+
+      </div>
+
+      <div className="header-center">
+        
+        <BsSearch className='search-icon'/>
+        <input type="text" placeholder='do you want this?' className='search-input'/>
+
+      </div>
+
+      <div className="header-right">
+        Login
+        <BiLogIn />
+
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Header
